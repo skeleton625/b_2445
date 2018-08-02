a07979fbf7cf83321d82288317e37c249b2ea905
WaniFi <skelpd24@gmail.com>
2018-08-02 19:00:49 +0900

    2445 문제 풀이 완료

--- baekjoon_2445/main.cpp
+++ baekjoon_2445/main.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 int main()
 {
+	int l;
+	cin >> l;
+	for (int i = 0; i < l; i++) {
+		for (int j = 0; j <= i; j++) cout << "*";
+		for (int j = 2 * (l - i-1); j > 0; j--) cout << " ";
+		for (int j = 0; j <= i; j++) cout << "*";
+		cout << endl;
+	}
+	for (int i = l-1; i > 0; i--) {
+		for (int j = i; j > 0; j--) cout << "*";
+		for (int j = 0; j < 2*(l-i); j++) cout << " ";
+		for (int j = i; j > 0; j--) cout << "*";
+		cout << endl;
+	}
 	return 0;
 }
